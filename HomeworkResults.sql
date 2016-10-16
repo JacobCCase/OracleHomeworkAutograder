38ce135b1ab8fcade2c91f79db2c771caf2350c3
JacobCCase <jacobcase@jacobcase.com>
2016-10-16 12:46:01 -0600

    Add files via upload

---
+++ HomeworkResults.sql
@@ -0,0 +1,16 @@
+
+CREATE TABLE HomeworkResults
+( UNID                  VARCHAR2(10 BYTE),
+  AssignmentNumber      NUMBER(2,0),
+  QuestionNumber        NUMBER(2,0),
+  Executes              CHAR(1 BYTE),
+  MeetsLogicCriteria    CHAR(1 BYTE),
+  ResultSetsMatch       CHAR(1 BYTE),
+  Score                 NUMBER(18,5),
+  InsertedTimeStamp     TIMESTAMP(0),
+  UpdatedTimeStamp      TIMESTAMP(0),
+  CONSTRAINT PK_HomeworkResults PRIMARY KEY ( UNID )
+) ;
+
+
+
